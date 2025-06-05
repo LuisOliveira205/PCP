4531f578aae1d5852689b67d5e28a3b1404ae26d
Pedro-Rodrigues-pog <pedro.rodrigues252@etec.sp.gov.br>
2025-06-05 20:34:30 -0300

    Update BancoPCP - adicionado comandos de conexão do banco tático + correção de erros

---
+++ BancoPCP/Class1.cs
@@ -0,0 +1,419 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace BancoPCP
+{
+    public class ConnBD
+    {
+        private static string conexao = "server=localhost;user=root;database=TOI;port=3306;password=root";
+
+        public void bancoObjetivo(string objetivo)
+        {
+            using (MySqlConnection conn = new MySqlConnection(conexao))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "insert into estrategicos (objetivo_es) values (@objetivo_es)";
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@objetivo_es", objetivo);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Erro: " + ex.Message);
+                }
+            }
+        }
+
+        public void bancoEstrategia(string estrategia)
+        {
+            using (MySqlConnection conn = new MySqlConnection(conexao))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "insert into estrategicos (estrategia_es) values (@estrategia_es)";
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@estrategia_es", estrategia);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Erro: " + ex.Message);
+                }
+            }
+        }
+
+        public void bancoRecursos(string equipamentos, int trabalhadores)
+        {
+            using (MySqlConnection conn = new MySqlConnection(conexao))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "insert into estrategicos (equipamentos_es, trabalhadores_es) values (@equipamentos_es, @trabalhadores_es)";
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@equipamentos_es", equipamentos);
+                        cmd.Parameters.AddWithValue("@trabalhadores_es", trabalhadores);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Erro: " + ex.Message);
+                }
+            }
+        }
+
+        public void bancoPortifolio(string portifolio)
+        {
+            using (MySqlConnection conn = new MySqlConnection(conexao))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "insert into estrategicos (portifolio_es) values (@portifolio_es)";
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@portifolio_es", portifolio);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Erro: " + ex.Message);
+                }
+            }
+        }
+
+        public void bancoLocal(string local)
+        {
+            using (MySqlConnection conn = new MySqlConnection(conexao))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "insert into estrategicos (local_es) values (@local_es)";
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@local_es", local);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Erro: " + ex.Message);
+                }
+            }
+        }
+
+        public void bancoRelacoes(string relacoes)
+        {
+            using (MySqlConnection conn = new MySqlConnection(conexao))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "insert into estrategicos (relacoes_es) values (@relacoes_es)";
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@relacoes_es", relacoes);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Erro: " + ex.Message);
+                }
+            }
+        }
+
+        public bool cadEstrategico(string email, string senha, string cpf)
+        {
+            using (MySqlConnection conn = new MySqlConnection(conexao))
+            {
+                try
+                {
+                    conn.Open();
+
+                    string checkQuery = "select count(*) from estrategicos where email_es = @email_es";
+                    using (MySqlCommand checkCmd = new MySqlCommand(checkQuery, conn))
+                    {
+                        checkCmd.Parameters.AddWithValue("@email_es", email);
+                        int count = Convert.ToInt32(checkCmd.ExecuteScalar());
+
+                        if (count > 0)
+                        {
+                            Console.WriteLine("Email já cadastrado.");
+                            return false;
+                        }
+                    }
+
+                    string insertQuery = "insert into estrategicos (email_es, senha_es, cpf_es) values (@email_es, @senha_es, @cpf_es)";
+                    using (MySqlCommand cmd = new MySqlCommand(insertQuery, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@email_es", email);
+                        cmd.Parameters.AddWithValue("@senha_es", senha);
+                        cmd.Parameters.AddWithValue("@cpf_es", cpf);
+                        int result = cmd.ExecuteNonQuery();
+                        return result > 0;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Erro: " + ex.Message);
+                    return false;
+                }
+            }
+        }
+
+        public bool logEstrategico(string email, string senha)
+        {
+            using (MySqlConnection conn = new MySqlConnection(conexao))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "select * from estrategicos where email_es = @email_es and senha_es = @senha_es";
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@email_es", email);
+                        cmd.Parameters.AddWithValue("@senha_es", senha);
+
+                        using (var reader = cmd.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                Console.WriteLine("Login bem-sucedido. Bem-vindo!");
+                                return true;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Email ou senha inválidos.");
+                                return false;
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Erro: " + ex.Message);
+                    return false;
+                }
+            }
+        }
+
+        public void bancoPlano(string plano)
+        {
+            using (MySqlConnection conn = new MySqlConnection(conexao))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "insert into taticos (plano_ta) values (@plano_ta)";
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@plano_ta", plano);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Erro: " + ex.Message);
+                }
+            }
+        }
+
+        public void bancoLotesCronograma(string lotes, string cronograma)
+        {
+            using (MySqlConnection conn = new MySqlConnection(conexao))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "insert into taticos (lotes_ta, cronograma_ta) values (@lotes_ta, @cronograma_ta)";
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@lotes_ta", lotes);
+                        cmd.Parameters.AddWithValue("@cronograma_ta", cronograma);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Erro: " + ex.Message);
+                }
+            }
+        }
+
+        public void bancoCapacidade(string ajuste)
+        {
+            using (MySqlConnection conn = new MySqlConnection(conexao))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "insert into taticos (ajuste_ta) values (@ajuste_ta)";
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@ajuste_ta", ajuste);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Erro: " + ex.Message);
+                }
+            }
+        }
+
+        public void bancoMateriais(string materiais)
+        {
+            using (MySqlConnection conn = new MySqlConnection(conexao))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "insert into taticos (materiais_ta) values (@materiais_ta)";
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@materiais_ta", materiais);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Erro: " + ex.Message);
+                }
+            }
+        }
+
+        public void bancoProcesso(string processo)
+        {
+            using (MySqlConnection conn = new MySqlConnection(conexao))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "insert into taticos (revisao_ta) values (@revisao_ta)";
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@revisao_ta", processo);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Erro: " + ex.Message);
+                }
+            }
+        }
+
+        public void bancoDesvios(string desvios)
+        {
+            using (MySqlConnection conn = new MySqlConnection(conexao))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "insert into taticos (desvios_ta) values (@desvios_ta)";
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@desvios_ta", desvios);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Erro: " + ex.Message);
+                }
+            }
+        }
+
+        public bool cadTatico(string email, string senha, string cpf)
+        {
+            using (MySqlConnection conn = new MySqlConnection(conexao))
+            {
+                try
+                {
+                    conn.Open();
+
+                    string checkQuery = "select count(*) from taticos where email_ta = @email_ta";
+                    using (MySqlCommand checkCmd = new MySqlCommand(checkQuery, conn))
+                    {
+                        checkCmd.Parameters.AddWithValue("@email_ta", email);
+                        int count = Convert.ToInt32(checkCmd.ExecuteScalar());
+
+                        if (count > 0)
+                        {
+                            Console.WriteLine("Email já cadastrado.");
+                            return false;
+                        }
+                    }
+
+                    string insertQuery = "insert into taticos (email_ta, senha_ta, cpf_ta) values (@email_ta, @senha_ta, @cpf_ta)";
+                    using (MySqlCommand cmd = new MySqlCommand(insertQuery, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@email_ta", email);
+                        cmd.Parameters.AddWithValue("@senha_ta", senha);
+                        cmd.Parameters.AddWithValue("@cpf_ta", cpf);
+                        int result = cmd.ExecuteNonQuery();
+                        return result > 0;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Erro: " + ex.Message);
+                    return false;
+                }
+            }
+        }
+
+        public bool logTatico(string email, string senha)
+        {
+            using (MySqlConnection conn = new MySqlConnection(conexao))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "select * from taticos where email_ta = @email_ta and senha_ta = @senha_ta";
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@email_ta", email);
+                        cmd.Parameters.AddWithValue("@senha_ta", senha);
+
+                        using (var reader = cmd.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                Console.WriteLine("Login bem-sucedido. Bem-vindo!");
+                                return true;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Email ou senha inválidos.");
+                                return false;
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Erro: " + ex.Message);
+                    return false;
+                }
+            }
+        }
+    }
+}
