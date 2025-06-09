f65fcca41d6f0dba72c5bc9254798391b9798b3e
Eduardo O.Andrade <eduardo.andrade41@etec.sp.gov.br>
2025-06-09 15:30:00 -0300

    Forms-PCP

---
+++ Form-PCP/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace PCP.Servico
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEstrategico = new System.Windows.Forms.Button();
+            this.btnTatico = new System.Windows.Forms.Button();
+            this.btnOperacional = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnEstrategico
+            // 
+            this.btnEstrategico.Location = new System.Drawing.Point(20, 50);
+            this.btnEstrategico.Name = "btnEstrategico";
+            this.btnEstrategico.Size = new System.Drawing.Size(100, 40);
+            this.btnEstrategico.TabIndex = 0;
+            this.btnEstrategico.Text = "Estratégico";
+            // 
+            // btnTatico
+            // 
+            this.btnTatico.Location = new System.Drawing.Point(140, 50);
+            this.btnTatico.Name = "btnTatico";
+            this.btnTatico.Size = new System.Drawing.Size(100, 40);
+            this.btnTatico.TabIndex = 1;
+            this.btnTatico.Text = "Tático";
+            // 
+            // btnOperacional
+            // 
+            this.btnOperacional.Location = new System.Drawing.Point(260, 50);
+            this.btnOperacional.Name = "btnOperacional";
+            this.btnOperacional.Size = new System.Drawing.Size(100, 40);
+            this.btnOperacional.TabIndex = 2;
+            this.btnOperacional.Text = "Operacional";
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(140, 120);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(100, 30);
+            this.btnSair.TabIndex = 3;
+            this.btnSair.Text = "Sair";
+            // 
+            // Menu
+            // 
+            this.ClientSize = new System.Drawing.Size(945, 336);
+            this.Controls.Add(this.btnEstrategico);
+            this.Controls.Add(this.btnTatico);
+            this.Controls.Add(this.btnOperacional);
+            this.Controls.Add(this.btnSair);
+            this.Name = "Menu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Menu Principal - PCP";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+    }
+}
+
---
+++ Form-PCP/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PCP.Servico
+{
+
+    public partial class Menu : Form
+    {
+        private Button btnEstrategico;
+        private Button btnTatico;
+        private Button btnOperacional;
+        private Button btnSair;
+
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            this.Text = "Menu Principal - PCP";
+            this.Size = new Size(400, 250);
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+            btnEstrategico = new Button();
+            btnEstrategico.Text = "Estratégico";
+            btnEstrategico.Size = new Size(100, 40);
+            btnEstrategico.Location = new Point(20, 50);
+            btnEstrategico.Click += BtnEstrategico_Click;
+            this.Controls.Add(btnEstrategico);
+
+            btnTatico = new Button();
+            btnTatico.Text = "Tático";
+            btnTatico.Size = new Size(100, 40);
+            btnTatico.Location = new Point(140, 50);
+            btnTatico.Click += BtnTatico_Click;
+            this.Controls.Add(btnTatico);
+
+            btnOperacional = new Button();
+            btnOperacional.Text = "Operacional";
+            btnOperacional.Size = new Size(100, 40);
+            btnOperacional.Location = new Point(260, 50);
+            btnOperacional.Click += BtnOperacional_Click;
+            this.Controls.Add(btnOperacional);
+
+            btnSair = new Button();
+            btnSair.Text = "Sair";
+            btnSair.Size = new Size(100, 30);
+            btnSair.Location = new Point(140, 120);
+            btnSair.Click += BtnSair_Click;
+            this.Controls.Add(btnSair);
+        }
+
+        private void BtnEstrategico_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            using (var form = new FormEstrategico())
+            {
+                form.ShowDialog();
+            }
+            this.Show();
+        }
+
+        private void BtnTatico_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            using (var form = new FormTatico())
+            {
+                form.ShowDialog();
+            }
+            this.Show();
+        }
+
+        private void BtnOperacional_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            using (var form = new FormOperacional())
+            {
+                form.ShowDialog();
+            }
+            this.Show();
+        }
+
+        private void BtnSair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+
+
+    public class FormEstrategico : Form
+    {
+        public FormEstrategico()
+        {
+            this.Text = "Formulário Estratégico";
+            this.Size = new Size(400, 300);
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+            Label lbl = new Label()
+            {
+                Text = "Bem-vindo ao módulo Estratégico",
+                AutoSize = true,
+                Location = new Point(20, 20),
+                Font = new Font("Arial", 14, FontStyle.Bold)
+            };
+
+            this.Controls.Add(lbl);
+        }
+    }
+
+
+    public class FormTatico : Form
+    {
+        public FormTatico()
+        {
+            this.Text = "Formulário Tático";
+            this.Size = new Size(400, 300);
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+            Label lbl = new Label()
+            {
+                Text = "Bem-vindo ao módulo Tático",
+                AutoSize = true,
+                Location = new Point(20, 20),
+                Font = new Font("Arial", 14, FontStyle.Bold)
+            };
+
+            this.Controls.Add(lbl);
+        }
+    }
+
+
+    public class FormOperacional : Form
+    {
+        public FormOperacional()
+        {
+            this.Text = "Formulário Operacional";
+            this.Size = new Size(400, 300);
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+            Label lbl = new Label()
+            {
+                Text = "Bem-vindo ao módulo Operacional",
+                AutoSize = true,
+                Location = new Point(20, 20),
+                Font = new Font("Arial", 14, FontStyle.Bold)
+            };
+
+            this.Controls.Add(lbl);
+        }
+    }
+}
